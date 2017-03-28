244435809f253457bff22f65a352b61cd84278d2
LNKLNK00 <lnklnk00@163.com>
2017-03-28 17:42:51 +0800

    修改登录拦截器

--- src/main/java/com/lnk/jxc/interceptor/LoginInterceptor.java
+++ src/main/java/com/lnk/jxc/interceptor/LoginInterceptor.java
@@ -6,23 +6,20 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import com.lnk.jxc.util.HttpRequestUtil;
 
 /**
- * 登陆状态拦截器
- * @author Administrator
+ * <p>title:登录拦截器</p>
+ * <p>description:</p>
+ * @author Nick Liu
+ * @createTime 2017年3月28日 下午5:02:55
  */
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final static Logger LOG = LoggerFactory.getLogger(LoginInterceptor.class);
-
-    @Autowired
-    private RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     /**
      * 验证是否登陆
@@ -31,21 +28,18 @@ public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         HttpSession session = request.getSession();
-        if (session.getAttribute("userId") == null) {
+        if (session.getAttribute("USERINFO") == null) {
             //不符合条件的，跳转到登录界面 
             boolean isAjax = HttpRequestUtil.isAjax(request);
-            LOG.info("isAjax------------" + isAjax);
+            LOG.info("isAjax=" + isAjax);
             if (isAjax) {
-                requestMappingHandlerMapping.getHandlerMethods();
-                //request.getRequestURI();
-                request.getRequestDispatcher("/login-need-login").forward(request, response);
+                request.getRequestDispatcher("/ajaxLogin").forward(request, response);
             } else {
-                request.getRequestDispatcher("/login-index.html").forward(request, response);
+                request.getRequestDispatcher("/login.html").forward(request, response);
             }
             return false;
-        } else {
-            return true;
         }
+        return true;
     }
 
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
