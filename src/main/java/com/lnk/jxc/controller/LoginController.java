107e9bd67e8d73d5a26cfdc73a8aea9f5cfb4683
LNKLNK00 <lnklnk00@163.com>
2017-03-31 22:06:36 +0800

    添加登出方法

--- src/main/java/com/lnk/jxc/controller/LoginController.java
+++ src/main/java/com/lnk/jxc/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -48,6 +49,12 @@ public class LoginController {
         }
         session.setAttribute("USERINFO", user);
         LOG.info("用户:" + user.getUsername() + " 登录成功，跳转到首页！");
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/logout.html")
+    public String logout(HttpSession session) {
+        session.removeAttribute("USERINFO");
         return "redirect:/index.html";
     }
 }
