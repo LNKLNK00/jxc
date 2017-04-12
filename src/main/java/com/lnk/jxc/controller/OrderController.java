b308d2d0a3385d794f1593813eee0088cdfd3d83
LNKLNK00 <lnklnk00@163.com>
2017-04-12 18:02:11 +0800

    初始化

---
+++ src/main/java/com/lnk/jxc/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.lnk.jxc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * <p>title:订单控制类</p>
+ * <p>description:</p>
+ * @author Nick Liu
+ * @createTime 2017年4月12日 下午4:44:51
+ */
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    private final static Logger LOG = LoggerFactory.getLogger(OrderController.class);
+
+}
