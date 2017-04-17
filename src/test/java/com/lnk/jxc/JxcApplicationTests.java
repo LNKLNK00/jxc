9a5bc450557a7ad3564bf7a715aa14f9dc7ffd90
LNKLNK00 <lnklnk00@163.com>
2017-04-17 18:38:55 +0800

    单元测试

--- src/test/java/com/lnk/jxc/JxcApplicationTests.java
+++ src/test/java/com/lnk/jxc/JxcApplicationTests.java
@@ -1,5 +1,7 @@
 package com.lnk.jxc;
 
+import java.text.DecimalFormat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class JxcApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        float scale = 34.234323f;
+        DecimalFormat fnum = new DecimalFormat("##0.00");
+        String dd = fnum.format(scale);
+        System.out.println(dd);
+    }
 
 }
