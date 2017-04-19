c77d72adb20ab2a40ece2b411750dfbac7cf9d85
LNKLNK00 <lnklnk00@163.com>
2017-04-19 18:00:25 +0800

    更新SQL

--- src/main/resources/sql/jxc.sql
+++ src/main/resources/sql/jxc.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-04-18 17:46:21
+Date: 2017-04-19 17:59:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,15 +29,18 @@ CREATE TABLE `order_info` (
   `user_id` tinyint(2) NOT NULL COMMENT '操作人ID',
   PRIMARY KEY (`id`),
   UNIQUE KEY `order_no` (`order_no`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of order_info
 -- ----------------------------
-INSERT INTO `order_info` VALUES ('2', 'NO1492417723368', '143.00', '74.00', '2017-04-05 16:28:43', 'admin', '1');
+INSERT INTO `order_info` VALUES ('2', 'NO1492417723368', '143.00', '74.00', '2017-03-03 16:28:43', 'admin', '1');
 INSERT INTO `order_info` VALUES ('3', 'NO1492418273313', '752.00', '194.00', '2017-04-07 16:37:53', 'admin', '1');
 INSERT INTO `order_info` VALUES ('4', 'NO1492425016400', '341.40', '140.40', '2017-04-17 18:30:16', 'admin', '1');
 INSERT INTO `order_info` VALUES ('5', 'NO1492504548469', '912.00', '264.00', '2017-04-18 16:35:48', 'admin', '1');
+INSERT INTO `order_info` VALUES ('6', 'NO1492594353417', '236.00', '125.00', '2017-04-19 17:32:33', 'admin', '1');
+INSERT INTO `order_info` VALUES ('7', 'NO1492594376570', '1290.00', '730.00', '2017-04-19 17:32:56', 'admin', '1');
+INSERT INTO `order_info` VALUES ('8', 'NO1492594448308', '485.00', '308.00', '2017-04-19 17:34:08', 'admin', '1');
 
 -- ----------------------------
 -- Table structure for order_product
@@ -47,14 +50,14 @@ CREATE TABLE `order_product` (
   `id` int(8) NOT NULL AUTO_INCREMENT COMMENT '序号',
   `order_id` int(8) NOT NULL COMMENT '订单ID',
   `product_id` int(8) NOT NULL COMMENT '产品ID',
-  `num` tinyint(3) NOT NULL COMMENT '购买数量',
+  `num` int(6) NOT NULL COMMENT '购买数量',
   `rate` float(3,2) NOT NULL DEFAULT '0.00' COMMENT '折扣利率',
   `price` float(7,2) NOT NULL COMMENT '售价',
   `bid` float(7,2) NOT NULL COMMENT '进价',
   PRIMARY KEY (`id`),
   KEY `order_id` (`order_id`),
   KEY `product_id` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of order_product
@@ -70,6 +73,15 @@ INSERT INTO `order_product` VALUES ('8', '4', '11', '2', '1.00', '76.00', '54.00
 INSERT INTO `order_product` VALUES ('9', '4', '3', '3', '1.00', '19.80', '10.00');
 INSERT INTO `order_product` VALUES ('10', '4', '2', '3', '1.00', '5.00', '1.00');
 INSERT INTO `order_product` VALUES ('11', '5', '11', '12', '1.00', '76.00', '54.00');
+INSERT INTO `order_product` VALUES ('12', '6', '4', '3', '1.00', '23.00', '12.00');
+INSERT INTO `order_product` VALUES ('13', '6', '5', '2', '1.00', '4.00', '3.00');
+INSERT INTO `order_product` VALUES ('14', '6', '6', '3', '1.00', '53.00', '23.00');
+INSERT INTO `order_product` VALUES ('15', '7', '11', '3', '1.00', '76.00', '54.00');
+INSERT INTO `order_product` VALUES ('16', '7', '10', '2', '1.00', '66.00', '54.00');
+INSERT INTO `order_product` VALUES ('17', '7', '9', '2', '1.00', '432.00', '123.00');
+INSERT INTO `order_product` VALUES ('18', '7', '8', '1', '1.00', '66.00', '44.00');
+INSERT INTO `order_product` VALUES ('19', '8', '7', '3', '1.00', '111.00', '23.00');
+INSERT INTO `order_product` VALUES ('20', '8', '11', '2', '1.00', '76.00', '54.00');
 
 -- ----------------------------
 -- Table structure for product
@@ -79,31 +91,32 @@ CREATE TABLE `product` (
   `id` int(8) NOT NULL AUTO_INCREMENT COMMENT '序号',
   `names` varchar(50) NOT NULL COMMENT '产品名称',
   `norms` varchar(20) NOT NULL COMMENT '规格',
-  `unit` char(1) NOT NULL COMMENT '单位',
+  `unit` char(6) NOT NULL COMMENT '单位',
   `bid` float(7,2) NOT NULL COMMENT '进价',
   `price` float(7,2) NOT NULL COMMENT '定价',
-  `num` tinyint(3) NOT NULL DEFAULT '0' COMMENT '库存数量',
+  `num` int(6) NOT NULL DEFAULT '0' COMMENT '库存数量',
   `factory` varchar(30) DEFAULT NULL COMMENT '生产厂家',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态(1：上架， 0：下架)',
   `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP COMMENT '添加时间',
   `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of product
 -- ----------------------------
 INSERT INTO `product` VALUES ('1', '感冒药', '25g*12袋/盒', '盒', '30.00', '60.00', '21', '湖南兽药厂', '1', '2017-04-17 16:28:43', '2017-04-17 16:28:43');
-INSERT INTO `product` VALUES ('2', '巨龙动保先乐10%阿莫西林可溶粉', '100克/袋', '袋', '1.00', '5.00', '4', '中国大陆', '1', '2017-04-18 15:03:21', '2017-04-18 15:03:21');
-INSERT INTO `product` VALUES ('3', '华畜鸡用鸡球虫散', '500g/袋', '袋', '10.00', '19.80', '2', '中国大陆', '1', '2017-04-18 15:03:42', '2017-04-18 15:03:42');
-INSERT INTO `product` VALUES ('4', 'aa', 'aa', 'a', '12.00', '23.00', '12', 'aa', '1', '2017-04-17 18:30:16', '2017-04-17 18:30:16');
-INSERT INTO `product` VALUES ('5', 'bb', 'bb', 'b', '3.00', '4.00', '1', 'b', '1', '2017-04-17 16:28:43', '2017-04-17 16:28:43');
-INSERT INTO `product` VALUES ('6', 'cccc', 'ccc', 'c', '23.00', '53.00', '4', 'c', '1', '2017-04-14 12:28:01', '2017-04-14 12:28:01');
-INSERT INTO `product` VALUES ('7', 'dddd', 'dd', 'd', '23.00', '111.00', '55', 'd', '1', '2017-04-14 12:28:08', '2017-04-14 12:28:08');
-INSERT INTO `product` VALUES ('8', 'eeeee', 'eee', 'e', '44.00', '66.00', '22', 'eeee', '1', '2017-04-14 12:28:17', '2017-04-14 12:28:17');
-INSERT INTO `product` VALUES ('9', 'ffff', 'ff', 'f', '123.00', '432.00', '6', 'ffffffff', '1', '2017-04-14 12:28:12', '2017-04-14 12:28:12');
-INSERT INTO `product` VALUES ('10', 'gggg', 'gg', 'g', '54.00', '66.00', '39', 'fff', '1', '2017-04-17 16:37:53', '2017-04-17 16:37:53');
-INSERT INTO `product` VALUES ('11', 'hhhh', 'hhh', 'h', '54.00', '76.00', '50', 'hhh', '1', '2017-04-18 16:35:48', '2017-04-18 16:35:48');
+INSERT INTO `product` VALUES ('2', '巨龙动保先乐10%阿莫西林可溶粉', '100克/袋', '袋', '1.00', '5.00', '237', '中国大陆', '1', '2017-04-19 17:32:02', '2017-04-19 17:32:02');
+INSERT INTO `product` VALUES ('3', '华畜鸡用鸡球虫散', '500g/袋', '袋', '10.00', '19.80', '25', '中国大陆', '1', '2017-04-19 17:23:38', '2017-04-19 17:23:38');
+INSERT INTO `product` VALUES ('4', 'aa', 'aa', 'a', '12.00', '23.00', '64', 'aa', '1', '2017-04-19 17:32:33', '2017-04-19 17:32:33');
+INSERT INTO `product` VALUES ('5', 'bb', 'bb', 'b', '3.00', '4.00', '53', 'b', '1', '2017-04-19 17:32:33', '2017-04-19 17:32:33');
+INSERT INTO `product` VALUES ('6', 'cccc', 'ccc', 'c', '23.00', '53.00', '101', 'c', '1', '2017-04-19 17:32:33', '2017-04-19 17:32:33');
+INSERT INTO `product` VALUES ('7', 'dddd', 'dd', 'd', '23.00', '111.00', '52', 'd', '1', '2017-04-19 17:34:08', '2017-04-19 17:34:08');
+INSERT INTO `product` VALUES ('8', 'eeeee', 'eee', 'e', '44.00', '66.00', '21', 'eeee', '1', '2017-04-19 17:32:56', '2017-04-19 17:32:56');
+INSERT INTO `product` VALUES ('9', 'ffff', 'ff', 'f', '123.00', '432.00', '27', 'ffffffff', '1', '2017-04-19 17:32:56', '2017-04-19 17:32:56');
+INSERT INTO `product` VALUES ('10', 'gggg', 'gg', 'g', '54.00', '66.00', '37', 'fff', '1', '2017-04-19 17:32:56', '2017-04-19 17:32:56');
+INSERT INTO `product` VALUES ('11', 'hhhh', 'hhh', 'h', '54.00', '76.00', '45', 'hhh', '1', '2017-04-19 17:34:08', '2017-04-19 17:34:08');
+INSERT INTO `product` VALUES ('12', '猪饲料', '50kg/袋', '袋', '200.00', '400.00', '40', '双胞胎', '1', '2017-04-19 17:09:18', '2017-04-19 17:09:18');
 
 -- ----------------------------
 -- Table structure for stock
@@ -113,13 +126,13 @@ CREATE TABLE `stock` (
   `id` int(8) NOT NULL AUTO_INCREMENT COMMENT '序号',
   `product_id` int(8) NOT NULL COMMENT '产品ID',
   `product_name` varchar(50) NOT NULL COMMENT '产品名称',
-  `num` tinyint(3) NOT NULL COMMENT '进货数量',
+  `num` int(6) NOT NULL COMMENT '进货数量',
   `create_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP COMMENT '进货时间',
   `user_name` varchar(10) NOT NULL COMMENT '操作人',
   `user_id` tinyint(2) NOT NULL COMMENT '操作人ID',
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of stock
@@ -135,6 +148,13 @@ INSERT INTO `stock` VALUES ('8', '9', 'ffff', '6', '2017-04-14 12:28:12', 'admin
 INSERT INTO `stock` VALUES ('9', '8', 'eeeee', '22', '2017-04-14 12:28:17', 'admin', '1');
 INSERT INTO `stock` VALUES ('10', '10', 'gggg', '46', '2017-04-14 12:28:25', 'admin', '1');
 INSERT INTO `stock` VALUES ('11', '11', 'hhhh', '66', '2017-04-14 12:29:26', 'admin', '1');
+INSERT INTO `stock` VALUES ('12', '12', '猪饲料', '40', '2017-04-19 17:09:18', 'admin', '1');
+INSERT INTO `stock` VALUES ('13', '3', '华畜鸡用鸡球虫散', '23', '2017-04-19 17:23:38', 'admin', '1');
+INSERT INTO `stock` VALUES ('14', '4', 'aa', '55', '2017-04-19 17:23:47', 'admin', '1');
+INSERT INTO `stock` VALUES ('15', '9', 'ffff', '23', '2017-04-19 17:23:50', 'admin', '1');
+INSERT INTO `stock` VALUES ('16', '5', 'bb', '54', '2017-04-19 17:23:54', 'admin', '1');
+INSERT INTO `stock` VALUES ('17', '6', 'cccc', '100', '2017-04-19 17:27:14', 'admin', '1');
+INSERT INTO `stock` VALUES ('18', '2', '巨龙动保先乐10%阿莫西林可溶粉', '233', '2017-04-19 17:32:02', 'admin', '1');
 
 -- ----------------------------
 -- Table structure for user
